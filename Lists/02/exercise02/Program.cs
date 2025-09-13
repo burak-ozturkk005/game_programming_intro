46cacf762285839b5074f62d4736f684707aa3f3
Burak Ozturk <burak.ozturkk05@gmail.com>
2025-09-13 13:42:33 +0300

    ex02

---
+++ Lists/02/exercise02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+List<int> sayilar = new List<int>();
+//mini uygulama
+// kullanici 5 sayi girsin bu sayilarin max min ini bul hepsini foreach ile dön.
+int sayac = 5;
+int sayi;
+bool basariliMi;
+while (sayac > 0)
+{
+    Console.WriteLine("Lutfen sayi girin: ");
+    basariliMi = int.TryParse(Console.ReadLine(), out sayi);
+    sayilar.Add(sayi);
+    sayac--;
+    
+}
+foreach (var item in sayilar)
+{
+    Console.WriteLine(item);
+}
+int max = sayilar[0];
+int min = sayilar[0];
+
+for (int i = 0; i < sayilar.Count; i++)
+{
+    if (sayilar[i] > max)
+    {
+        max = sayilar[i];
+    }
+    else if (sayilar[i] < min)
+    {
+        min = sayilar[i];
+    }
+}
+
+Console.WriteLine("En büyük sayi: " + max);
+Console.WriteLine("En küçük sayi: " + min);
+//LINQ ile bulma
+int max1 = sayilar.Max();
+int min1 = sayilar.Min();
+double ortalama = sayilar.Average();
+int toplam = sayilar.Sum();
+Console.WriteLine(toplam);
+Console.WriteLine(ortalama);
